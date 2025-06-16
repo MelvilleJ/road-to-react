3a27e7ce0a183a265736e7d5509c7dab59972716
MelvilleJ <103816650+MelvilleJ@users.noreply.github.com>
2025-06-16 10:00:12 -0400

    Update promisetest.js

--- Javascript/promisetest.js
+++ Javascript/promisetest.js
@@ -2,7 +2,31 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-delay(3000).then(() => {
+function fetchData(name) {
+  return new Promise((resolve, reject) => {
+    if (!name){
+        reject(new Error("Name is required"));
+    }else if (typeof name !== 'string') {
+        reject(new Error("Name must be a string"));
+    }else {
+        console.log(`Fetching data for ${name}...`);
+    setTimeout(() => {
+      resolve("Data fetched successfully!");
+    }, 2000);
+    }
+  });
+}
+
+fetchData("John Doe")
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("Error:", error.message);
+  });
+
+
+/*delay(3000).then(() => {
   console.log("3 seconds passed!");
 });
 
@@ -10,4 +34,5 @@ console.log("This message is logged immediately.");
 console.log("Waiting for 3 seconds...");
 delay(1500).then(() => {
   console.log("This message is logged during the delay.");
-});
+});*/
+
