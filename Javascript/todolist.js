c61460ed0662de0bb2ef6ccc133598f08dd9a057
MelvilleJ <103816650+MelvilleJ@users.noreply.github.com>
2025-06-16 08:07:47 -0400

    Update todolist.js

--- Javascript/todolist.js
+++ Javascript/todolist.js
@@ -1,6 +1,7 @@
 // todolist.js
 
-
+// This script manages a simple todo list with functions to add, display, and remove todos.
+// Initial todo list
 let todoList = [
     {
         id: 1,
@@ -23,10 +24,13 @@ let todoList = [
         completed: true
     }
 ];
+const info = {id: 1, name: "Todo List Manager", version: "1.0.0"};
+const {name} = info;
+const {version} = info;
 
-
-
-
+console.log(`App Name: ${name}, Version: ${version}`);
+// Functions to manipulate the todo list
+// Add a new todo item
 function addTodo() {
     const newTodo = {
         id: 5,
@@ -36,22 +40,23 @@ function addTodo() {
     todoList.push(newTodo);
     console.log("Todo added:", newTodo);
 }
-
+// Display all todo items
 function displayTodos() {
     console.log("Todo List:");
     todoList.forEach(todo => {
         console.log(`ID: ${todo.id}, Title: ${todo.title}, Completed: ${todo.completed}`);
     });
 }
-
+// Remove a todo item by ID
 function removeTodo(id) {
     todoList = todoList.filter(todo => todo.id !== id);
     console.log(`Todo with ID ${id} removed.`);
 }
-
-
+// Example usage
 displayTodos();
 addTodo();
 displayTodos();
 removeTodo(2);
 displayTodos();
+
+console.log(`End of Demo: ${name} , ${version}`);
